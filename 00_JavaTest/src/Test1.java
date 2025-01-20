225ff1f635d9adecb4d199094563f145f0aed519
kkm <rhksalsdldia@naver.com>
2025-01-20 11:25:26 +0900

    java workspace 등록

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,43 @@
+// JS와 주석 동일
+
+/*
+ * Run 버튼 클릭 시 (단축키 ctrl + f11)
+ * 1) 자동으로 컴파일 수행(javac xxx.java) => .class 파일 생성
+ * 	=> .class 파일(byte code) 생성
+ * 
+ * 2) 컴파일 완료 후 .class 파일 실행(java class파일명)
+ * 
+ */
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("이클립스 실행중");
+		System.out.println("졸리다");
+		System.out.println("집가고 싶다");
+
+		/*	
+		 * Java API DOC : Java에서 제공해주는 코드의 사용법이 설명 되어있는 문서
+		 * 
+		 * DOC 확인 방법
+		 * 1) google => java 21 api 검색
+		 * 
+		 * 2) 궁금한 코드 클릭 => shift + f2
+		 */
+		
+		/*		
+		 * git 관련 안올리는 친구들
+		 * 
+		 * .metadata 폴더 : 이클립스 설정 모음 폴더인데 서로 호환이 안될수도 있어서 올리지 않는다.
+		 * 
+		 * .classpath 폴더 : 클래스 경로 인식 방법 설정 폴더 
+		 * 
+		 * bin 폴더 : 컴파일된 파일(바이트 코드) 모인 폴더
+		 * 
+		 * .class 파일 : 컴파일된 파일(바이트 코드)
+		 * 
+		*/
+
+	}
+}
